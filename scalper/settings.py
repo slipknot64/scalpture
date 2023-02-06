82f04bfb1d407eed66c6eebe4e95572f58fb250f
slipknot64 <adilgreet@gmail.com>
2023-02-06 17:58:17 +0000

    updated settings

--- scalper/settings.py
+++ scalper/settings.py
@@ -89,12 +89,11 @@ if DEVELOPMENT_MODE is True:
             "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
         }
     }
-else:
+elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
     if os.getenv("DATABASE_URL", None) is None:
         raise Exception("DATABASE_URL environment variable not defined")
-
     DATABASES = {
-        "default": dj_database_url.config(os.environ.get("DATABASE_URL")),
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
     }
 
 
