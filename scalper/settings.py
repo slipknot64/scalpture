5f2737a36851cb75eb6bdab064425197f0400287
slipknot64 <adilgreet@gmail.com>
2023-02-06 00:34:44 +0000

    updated settings.py

--- scalper/settings.py
+++ scalper/settings.py
@@ -93,7 +93,7 @@ elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
     if os.getenv("DATABASE_URL", None) is None:
         raise Exception("DATABASE_URL environment variable not defined")
     DATABASES = {
-        "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
+        "default": dj_database_url.config(os.environ.get("DATABASE_URL")),
     }
 
 
