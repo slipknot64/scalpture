5a932c2de4eaa2ce7eba754f8769dcf889a9aa9a
slipknot64 <adilgreet@gmail.com>
2023-02-08 20:57:47 +0000

    updated settings

--- scalper/settings.py
+++ scalper/settings.py
@@ -12,10 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-from django.core.management.utils import get_random_secret_key
 from pathlib import Path
 import os
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
