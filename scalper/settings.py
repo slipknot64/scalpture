76d1a1a3c544cb3dd3479522e6e53f3ade416850
slipknot64 <adilgreet@gmail.com>
2023-02-09 21:56:33 +0000

    updated settings.py

--- scalper/settings.py
+++ scalper/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '178.62.13.58']
+ALLOWED_HOSTS = ['178.62.13.58']
 
 # Application definition
 
