8df35e5dcd22eae50328ab9740a770476afb4a65
slipknot64 <adilgreet@gmail.com>
2023-02-09 23:00:49 +0000

    updated game.py

--- scalpture/game.py
+++ scalpture/game.py
@@ -6,19 +6,11 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
-from selenium.webdriver.chrome.options import Options
 import time
-
-options = Options()
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-options.add_argument('--disable-gpu')
-options.add_argument('--remote-debugging-port=9222')
 
 def do_purchase(email, password, product_url, cvv):
     # Start a webdriver instance using the desired capabilities
-    driver = webdriver.Remote(command_executor='http://178.62.13.58:9515', options=options)
+    driver = webdriver.Chrome(desired_capabilities={"chromeOptions": {"binary": "/snap/chromium//2295/usr/lib/chromium-browser/chromedriver", "args": ["--remote-debugging-port=9515"]}}, service_url="http://178.62.13.58:9515/wd/hub")
     while True:
         try:
             # Navigate to the website you want to scrape product page
