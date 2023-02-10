931d58d1ba151bd0cb3c3f9463a5fa859144cba3
slipknot64 <adilgreet@gmail.com>
2023-02-10 00:35:50 +0000

    updated game.py

--- scalpture/game.py
+++ scalpture/game.py
@@ -10,7 +10,6 @@ from selenium.webdriver.chrome.options import Options
 import time
 
 chrome_options = Options()
-chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 
