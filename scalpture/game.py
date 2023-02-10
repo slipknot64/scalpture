e40fcd474b7712a5613880ece3466b9735a74524
slipknot64 <adilgreet@gmail.com>
2023-02-10 00:03:45 +0000

    updated game.py

--- scalpture/game.py
+++ scalpture/game.py
@@ -10,7 +10,7 @@ import time
 
 def do_purchase(email, password, product_url, cvv):
     # Start a webdriver instance using the desired capabilities
-    driver = webdriver.Remote(command_executor='http://178.62.13.58:9515')
+    driver = webdriver.Chrome(executable_path='/home/adil/scalpture/scalpture/chromedriver')
     while True:
         try:
             # Navigate to the website you want to scrape product page
