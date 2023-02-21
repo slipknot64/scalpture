3b11a5cb07a35e9f87758d4d4b20a5c3eb80d1b0
slipknot64 <adilgreet@gmail.com>
2023-02-21 21:56:54 +0000

    updated django key to be hidden and updated game.html background to include a dynamic wallpaper

--- scalper/settings.py
+++ scalper/settings.py
@@ -17,7 +17,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q(uc%j!r6yfs2ly6i9tiy@_hfrw@rrfc7okj&p(jnvcxk+dqm4'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
